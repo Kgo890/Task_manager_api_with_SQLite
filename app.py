812668e381d8f28b38f0a1e424cb2abe0769c15c
Kgo890 <gorekaden501@gmail.com>
2025-06-09 18:44:04 -0700

    Configure SQLAlchemy in Flask

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 # main flask app
 
 from flask import Flask
+# from config import db_url
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
+app.config['SQlALCHEMY_TRACK_MODIFICATIONS'] = False
 
-@app.route('/')
-def check():
-    return 'Hello world'
-
+db = SQLAlchemy(app)
 
 if __name__ == '__main__':
-    app.run()
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
