4e610a0a06243c0d030c6e4f3f07e6b60dbb0ebb
Kgo890 <gorekaden501@gmail.com>
2025-06-10 18:09:33 -0700

    made the model for the tasks like title,description, status, creation_time and due_time

--- models.py
+++ models.py
@@ -1,1 +1,13 @@
 # database models
+from app import db
+from datetime import datetime
+
+
+class Task(db.Model):
+    __tablename__ = "tasks"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(255), nullable=True)  # optional for users
+    status = db.Column(db.Boolean, default=False)  # True = done, False = not done
+    creation_time = db.Column(db.Datetime, default=datetime.utcnow)
+    due_date = db.Column(db.Datetime, nullable=True)  # optional for users
