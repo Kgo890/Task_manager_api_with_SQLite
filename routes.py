3cdf7bbf7d617a233b429b4655cde1fd49c53035
Kgo890 <gorekaden501@gmail.com>
2025-06-11 19:20:35 -0700

    made functions to update a task using their id, also a function to delete a task from the sqlite db and started on a function to filter task

--- routes.py
+++ routes.py
@@ -19,14 +19,49 @@ def create_tasks():
     data = request.get_json()
     title = data.get("title")
     description = data.get("description")
-    status = data.get("status",False)
+    status = data.get("status", False)
     due_date = data.get("due_date")
     if due_date:
         due_date = datetime.fromisoformat(due_date)
     else:
-        due_date =  None
+        due_date = None
 
     task = Task(title, description, status, due_date)
     db.session.add(task)
     db.session.commit()
     return jsonify(task.to_dict())
+
+
+@tasks_bp.route("/tasks/<int:task_id>", methods=["PUT"])
+def update_task(task_id):
+    data = request.get_json()
+    task = Task.query.get(task_id)
+    title = data.get("title")
+    description = data.get("description")
+    status = data.get("status", False)
+    due_date = data.get("due_date")
+    if task is None:
+        return jsonify({"error": "Task not found"})
+    else:
+        task.title = title
+        task.description = description
+        task.status = status
+        task.due_date = due_date
+        db.session.commit()
+    return jsonify(task.to_dict())
+
+
+@tasks_bp.route("/tasks/<int:task_id>", methods=["DELETE"])
+def delete_task(task_id):
+    task = Task.query.get(task_id)
+    if task is None:
+        return jsonify({"error": "Task not found"})
+    else:
+        db.session.delete(task)
+        db.session.commit()
+    return jsonify({"message": "Task deleted successfully"})
+
+
+@tasks_bp.route("/tasks/filter", methods=["GET"])
+def filtering_task():
+    pass
